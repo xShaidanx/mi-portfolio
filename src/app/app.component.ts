c14f076a2c32b30e648bbb345029223ebc814e59
Miguel Alvarez <xshaidanx@gmail.com>
2025-02-05 22:44:11 +0100

    preparando la estructura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { CalendarModule } from 'primeng/calendar';
+import { CommonModule } from '@angular/common';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, InputTextModule, ButtonModule, MessageModule, FormsModule, CalendarModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+  text = '';
+  date = Date();
+  msg = '';
+
+  constructor(private primeNGConfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.primeNGConfig.ripple = true;
+  }
+
+  onClick() {
+    this.msg = 'Welcome ' + this.text;
+  }
 }
